64cad562b5cfbc733521c3ea35670046b76778b5
yaswanththunti-hub <yaswanthyadavthunti@gmail.com>
2026-08-13 22:33:04 +0530

    Add files via upload

---
+++ arrays/sum of 2 elements in array.py
@@ -0,0 +1,10 @@
+class Solution: 
+    def twoSum(self, nums,target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return[i,j]
+s1=Solution()
+arr=[1,2,3,4,5]
+target=5
+print(s1.twoSum(arr,target))
