5cbae0b3f5dd57c145685b34e856632efd3cc3c2
Thunti Yaswanth Yadav <yaswanthyadavthunti@gmail.com>
2026-09-03 21:39:09 +0530

    stringconcept.py

--- basices/stringindexing.py
+++ basices/stringindexing.py
@@ -7,3 +7,29 @@ print(str[-5])
 print(str[4])
 for i in str:
     print(i)
+# str1="Thannu"
+# str2="Tikku"
+# str3=str1+str2
+# print(str3)
+# # str4=str1-str2
+# # print(str4)
+# # str5=str1*str2
+# # print(str5)
+# str6=str1*3
+# print(str6)
+# # str7=str1/str2
+# # print(str7)
+# str=input("enter the string :")
+# result=""
+# for i in str:
+#     if i not in result:
+#         result +=i
+# print(result)
+str="   tikku loves anuskha  "
+print(str)
+str1=str.lstrip()
+print(str1)
+str2=str.rstrip()
+print(str2)
+str3=str.strip()
+print(str3)
