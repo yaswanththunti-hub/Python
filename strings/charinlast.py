332ab824496b46cf8d58c65639f1c230d41040ba
Thunti Yaswanth Yadav <yaswanthyadavthunti@gmail.com>
2026-09-10 14:45:09 +0530

    char_atlast.py

--- strings/charinlast.py
+++ strings/charinlast.py
@@ -1,11 +1,18 @@
 str=input("enter a string :")
 str1=""
 str2=""
-result=""
 for i in str:
     if i=="a":
         str1=str1+i
     else:
         str2=str2+i
-    result=str2+str1
-print(result)
+print(str2+str1)
+
+# str=input("enter the string :")
+# ans=""
+# for i in str:
+#     if i!="a":
+#         ans=ans+i
+# while(len(ans)<len(str)):
+#     ans=ans+"a"
+# print(ans)
