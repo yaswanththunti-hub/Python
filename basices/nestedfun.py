c723429e24bf067acb85e4c8b4565645b0c50bb4
Thunti Yaswanth Yadav <yaswanthyadavthunti@gmail.com>
2026-09-22 23:16:06 +0530

    nestedfun.py

--- basices/nestedfun.py
+++ basices/nestedfun.py
@@ -4,3 +4,14 @@ def outer():
         print("Inside inner")#local variables 
     inner()
 outer()
+
+#legb
+from math import pi
+#pi=10
+def outer():
+    #pi=15
+    def inner():
+        #pi=20
+        print(pi)
+    inner()
+outer()
